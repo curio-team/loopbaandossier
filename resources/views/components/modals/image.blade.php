a061645d0cc3f2a65055fd2df1df4cd4b40f0950
Steven van Rosendaal <svanrosendaal@hotmail.com>
2022-11-28 18:10:52 +0100

    fixed css error

--- resources/views/components/modals/image.blade.php
+++ resources/views/components/modals/image.blade.php
@@ -3,7 +3,7 @@
 </button>
 
 <!-- {{$modalId}} -->
-<div id="{{ $modalId }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-hidden overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal h-full flex justify-center items-center bg-opacity-70 bg-black">
+<div id="{{ $modalId }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-hidden overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal h-full justify-center items-center bg-opacity-70 bg-black">
     <div class="relative max-h-7xl max-w-7xl md:h-auto">
         <div class="p-6 space-y-6">
             <img src="{{ $imageSrc }}">
@@ -14,9 +14,11 @@
 <script type="text/javascript">
     document.getElementById("{{ $modalId }}-button").addEventListener("click", function () {
         document.getElementById("{{ $modalId }}").classList.remove("hidden");
+        document.getElementById("{{ $modalId }}").classList.add("flex");
     });
 
     document.getElementById("{{ $modalId }}").addEventListener("click", function () {
         document.getElementById("{{ $modalId }}").classList.add("hidden");
+        document.getElementById("{{ $modalId }}").classList.remove("flex");
     });
 </script>
