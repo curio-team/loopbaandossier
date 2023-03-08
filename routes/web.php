b107f1fd46a3d37c85229530c795c0baa5d8dd4e
Steven van Rosendaal <svanrosendaal@hotmail.com>
2023-03-08 10:36:24 +0100

    removed test route

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'showHome'])->name('home');
-Route::get('test/{studentSlug}', [ManageController::class, 'test'])->name('test');
 
 require __DIR__.'/auth.php';
 
