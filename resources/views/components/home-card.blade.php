81bbc714cc7b64fd33d624c943714932341c51f0
Steven van Rosendaal <svanrosendaal@hotmail.com>
2023-03-13 11:40:47 +0100

    added header text underneath the main view buttons

--- resources/views/components/home-card.blade.php
+++ resources/views/components/home-card.blade.php
@@ -4,5 +4,6 @@
     @else
         <img src="{{ asset('assets/images/default.jpg') }}" alt="{{ $title }}" class="h-40 w-40 object-cover"><br>
     @endif
-    <a href="{{ route($route, $student->slug) }}" class="border rounded-md border-white text-white w-full inline-block text-center py-2">{{ $title }}</a>
+    <a href="{{ route($route, $student->slug) }}" class="border rounded-md border-white text-white w-full inline-block text-center py-2 mb-2">{{ $title }}</a>
+    <p class="text-center text-white w-40 text-sm">{{ $subtext }}</p>
 </div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="content-home font-sans">
         <div class="container flex justify-center mx-auto py-6">
-            @include('components.home-card', ['student' => $student, 'route' => 'introduction', 'title' => 'Voorstellen', 'image' => $student->pages->introduction_content_image])
-            @include('components.home-card', ['student' => $student, 'route' => 'qualities', 'title' => 'Kwaliteiten', 'image' => $student->pages->qualities_content_image])
-            @include('components.home-card', ['student' => $student, 'route' => 'motives', 'title' => 'Motieven', 'image' => $student->pages->motives_content_image])
-            @include('components.home-card', ['student' => $student, 'route' => 'exploration', 'title' => 'Werkexploratie', 'image' => $student->pages->exploration_content_image])
-            @include('components.home-card', ['student' => $student, 'route' => 'experience', 'title' => 'Loopbaansturing', 'image' => $student->pages->experience_content_image])
-            @include('components.home-card', ['student' => $student, 'route' => 'networks', 'title' => 'Netwerken', 'image' => $student->pages->networks_content_image])
+            @include('components.home-card', ['student' => $student, 'route' => 'introduction', 'title' => 'Voorstellen', 'image' => $student->pages->introduction_content_image, 'subtext' => $student->pages->introduction_header_title])
+            @include('components.home-card', ['student' => $student, 'route' => 'qualities', 'title' => 'Kwaliteiten', 'image' => $student->pages->qualities_content_image, 'subtext' => $student->pages->qualities_header_title])
+            @include('components.home-card', ['student' => $student, 'route' => 'motives', 'title' => 'Motieven', 'image' => $student->pages->motives_content_image, 'subtext' => $student->pages->motives_header_title])
+            @include('components.home-card', ['student' => $student, 'route' => 'exploration', 'title' => 'Werkexploratie', 'image' => $student->pages->exploration_content_image, 'subtext' => $student->pages->exploration_header_title])
+            @include('components.home-card', ['student' => $student, 'route' => 'experience', 'title' => 'Loopbaansturing', 'image' => $student->pages->experience_content_image, 'subtext' => $student->pages->experience_header_title])
+            @include('components.home-card', ['student' => $student, 'route' => 'networks', 'title' => 'Netwerken', 'image' => $student->pages->networks_content_image, 'subtext' => $student->pages->networks_header_title])
         </div>
     </div>
 @endsection
